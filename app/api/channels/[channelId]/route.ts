1702c46ded1ce8f8a2e4d2b7a59f6b0a6bef8eb8
hung dao <hungpepi2001@gmail.com>
2023-11-30 23:58:29 +0700

    delete server channel

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -3,18 +3,21 @@ import { db } from "@/lib/db";
 import { MemberRole } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function DELETE(
+  req: Request,
+  { params }: { params: { channelId: string } }
+) {
   try {
-    const { name, type } = await req.json();
     const { searchParams } = new URL(req.url);
 
     const serverId = searchParams.get("serverId");
     const profile = await currentProfile();
+
     if (!profile) return new NextResponse("Unauthorized", { status: 401 });
     if (!serverId)
       return new NextResponse("Server ID Missing", { status: 400 });
-    if (name === "general")
-      return new NextResponse("Name cannot be 'general'", { status: 400 });
+    if (!params.channelId)
+      return new NextResponse("Channel ID Missing", { status: 400 });
 
     const server = await db.server.update({
       where: {
@@ -30,17 +33,18 @@ export async function POST(req: Request) {
       },
       data: {
         channels: {
-          create: {
-            profileId: profile.id,
-            name,
-            type,
+          delete: {
+            id: params.channelId,
+            name: {
+              not: "general",
+            },
           },
         },
       },
     });
     return NextResponse.json(server);
   } catch (error) {
-    console.log("[CHANNELS_POST]", error);
+    console.log("[CHANNEL_ID_DELETE]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
